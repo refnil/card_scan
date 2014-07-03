26b487d3dc161bb11ea6e4650648482b8506a97b
Mélodie Lapointe <mlapointe923@hotmail.com>
2014-07-02 22:41:04 -0400

    Reindentation of scan_card.py

--- scan_card.py
+++ scan_card.py
@@ -25,133 +25,139 @@ from cv_utils import float_version, show_scaled, sum_squared, ccoeff_normed
 import config
 
 def get_card(color_capture, corners):
-	target = [(0,0), (223,0), (223,310), (0,310)]
-	#mat = cv.CreateMat(3,3, cv.CV_32FC1)
-	mat = cv2.getPerspectiveTransform(corners, target)
-	#warped = cv.CloneImage(color_capture)
+    target = [(0,0), (223,0), (223,310), (0,310)]
+    #mat = cv.CreateMat(3,3, cv.CV_32FC1)
+    mat = cv2.getPerspectiveTransform(corners, target)
+    #warped = cv.CloneImage(color_capture)
         #cv.WarpPerspective(color_capture, warped, mat)
-        warped = cv2.warpPerspective(color_capture, mat, color_capture.shape)
-	#cv.SetImageROI(warped, (0,0,223,310) )
-        warped = warped[0:310, 0:223]
-	return warped
+    warped = cv2.warpPerspective(color_capture, mat, color_capture.shape)
+    #cv.SetImageROI(warped, (0,0,223,310) )
+    warped = warped[0:310, 0:223]
+    return warped
 
 #*****************
 #this is the watch-for-card bit
 captures = []
 
 def card_window_clicked(event, x, y, flags, param):
-	if event == 6:
-	#delete capture array indexed at param, update windows
-		global captures
-		del captures[param]
-		update_windows()
+    if event == 6:
+    #delete capture array indexed at param, update windows
+        global captures
+        del captures[param]
+        update_windows()
 
 def update_windows(n=3):
-	#print "update windows!"
-	l = len(captures)
-	for i in xrange(1,min(n,l)+1):
-		#print "setting ",i
-		tmp = captures[-i].copy()
-                cv2.putText(tmp, "%s" % (l-i+1), (1,24), font,1.0, (255,255,255))
-		cv2.imshow("card_%d" % i, tmp)
-		cv2.setMouseCallback("card_%d" % i, card_window_clicked, l - i)
+    #print "update windows!"
+    l = len(captures)
+    for i in xrange(1,min(n,l)+1):
+        #print "setting ",i
+        tmp = captures[-i].copy()
+        cv2.putText(tmp, "%s" % (l-i+1), (1,24), font,1.0, (255,255,255))
+        cv2.imshow("card_%d" % i, tmp)
+        cv2.setMouseCallback("card_%d" % i, card_window_clicked, l - i)
 
 def watch_for_card(camera):
-	has_moved = False
-	been_to_base = False
+    has_moved = False
+    been_to_base = False
 
-	global captures
-	global font
-	captures = []
+    global captures
+    global font
+    captures = []
 
-	font = cv2.FONT_HERSHEY_SIMPLEX
-	retval, img = camera.read()
-	size = img.shape
-	n_pixels = size[0]*size[1]
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    retval, img = camera.read()
+    #print('img '+str(img))
+    size = img.shape
+    n_pixels = size[0]*size[1]
 
-	#grey = cv2.createImage(size, 8,1)
-	grey = numpy.zeros((size[0],size[1],3),numpy.uint8)
-	recent_frames = [grey.copy()]
-	base = grey.copy()
-	#cv.CvtColor(img, base, cv.CV_RGB2GRAY)
-        img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY) 
-	#cv.ShowImage('card', base)
-	#tmp = cv.CloneImage(grey)
+    #grey = cv2.createImage(size, 8,1)
+    grey = numpy.zeros((size[0],size[1]),numpy.uint8)
+    #print('gris'+str(grey))
+    #cv.CvtColor(img, base, cv.CV_RGB2GRAY)
+    base = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    recent_frames = [base.copy()]
+    print('base: '+str(base))
+    #cv.ShowImage('card', base)
+    #tmp = cv.CloneImage(grey)
 
 
-	while True:
-		retvalue, img = camera.read()
-		img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    while True:
+        retvalue, img = camera.read()
+        if img==None: 
+             break
+        img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
        #Rendu la a modifier#Rendu la a modifier#Rendu la a modifier 
-		biggest_diff = max(sum_squared(grey, frame) / n_pixels for frame in recent_frames)
+        biggest_diff = max(sum_squared(img_gray, frame) / n_pixels for frame in recent_frames)
 
-		#display the cam view
-		cv2.putText(img, "%s" % biggest_diff, (1,24), font,1.0, (255,255,255))
-		cv2.imshow('win',img)
-		recent_frames.append(grey.copy())
-		if len(recent_frames) > 3:
-			del recent_frames[0]
+        #display the cam view
+        cv2.putText(img, "%s" % biggest_diff, (1,24), font,1.0, (255,255,255))
+        cv2.imshow('win',img)
+        recent_frames.append(img_gray.copy())
+        if len(recent_frames) > 3:
+            del recent_frames[0]
 
-		#check for keystroke
-		c = cv2.waitKey(10)
-		#if there was a keystroke, reset the last capture
-		if c == 27:
-			return captures
-		elif c == 32:
-			has_moved = True
-			been_to_base = True
-		elif c == 114:
-			base = grey.copy()
+        #check for keystroke
+        c = cv2.waitKey(10)
+        #if there was a keystroke, reset the last capture
+        if c == 27:
+            return captures
+        elif c == 32:
+            has_moved = True
+            been_to_base = True
+        elif c == 114:
+            base = img_gray.copy()
 
 
-		#if we're stable-ish
-		if biggest_diff < 10:
-			#if we're similar to base, update base
-			#else, check for card
-			#base_diff = max(sum_squared(base, frame) / n_pixels for frame in recent_frames)
-			base_corr = min(ccoeff_normed(base, frame) for frame in recent_frames)
-			#cv.ShowImage('debug', base)
+        #if we're stable-ish
+        if biggest_diff < 10:
+            #if we're similar to base, update base
+            #else, check for card
+            #base_diff = max(sum_squared(base, frame) / n_pixels for frame in recent_frames)
+            #print('frame: '+str(frame.shape))
+            base_corr = min(ccoeff_normed(base, frame) for frame in recent_frames)
+            print('base_corr: '+str(base_corr))
+            #cv.ShowImage('debug', base)
 
-			"""for i, frame in enumerate(recent_frames):
-				tmp = cv.CloneImage(base)
-				cv.Sub(base, frame, tmp)
-				cv.Pow(tmp, tmp, 2.0)
-				cv.PutText(tmp, "%s" % (i+1), (1,24), font, (255, 255, 255))
-				#my_diff = sum_squared(base, frame) / n_pixels
-				my_diff = ccoeff_normed(base, frame) #score(base, frame, cv.CV_TM_CCOEFF_NORMED)
-				cv.PutText(tmp, "%s" % my_diff, (40, 24), font, (255, 255, 255))
-				cv.ShowImage('dbg%s' % (i+1), tmp)"""
-			#print "stable. corr = %s. moved = %s. been_to_base = %s" % (base_corr, has_moved, been_to_base)
-			if base_corr > 0.75 and not been_to_base:
-				base = grey.copy()
-			#	cv.ShowImage('debug', base)
-				has_moved = False
-				been_to_base = True
-				print "STATE: been to base. waiting for move"
-			elif has_moved and been_to_base:
-				corners = detect_card(grey, base)
-				if corners is not None:
-					card = get_card(grey, corners)
-					card = cv2.flip(card,-1)
-					captures.append(card)
-					update_windows()
-					#cv.ShowImage('card', card)
-					has_moved = False
-					been_to_base = False
-					print "STATE: detected. waiting for go to base"
-		else:
-			if not has_moved:
-				print "STATE: has moved. waiting for stable"
-			has_moved = True
+            """for i, frame in enumerate(recent_frames):
+                tmp = cv.CloneImage(base)
+                cv.Sub(base, frame, tmp)
+                cv.Pow(tmp, tmp, 2.0)
+                cv.PutText(tmp, "%s" % (i+1), (1,24), font, (255, 255, 255))
+                #my_diff = sum_squared(base, frame) / n_pixels
+                my_diff = ccoeff_normed(base, frame) #score(base, frame, cv.CV_TM_CCOEFF_NORMED)
+                cv.PutText(tmp, "%s" % my_diff, (40, 24), font, (255, 255, 255))
+                cv.ShowImage('dbg%s' % (i+1), tmp)"""
+            #print "stable. corr = %s. moved = %s. been_to_base = %s" % (base_corr, has_moved, been_to_base)
+            if base_corr > 0.75 and not been_to_base:
+                base = img_gray.copy()
+            #    cv.ShowImage('debug', base)
+                has_moved = False
+                been_to_base = True
+                print "STATE: been to base. waiting for move"
+            elif has_moved and been_to_base:
+                corners = detect_card(img_gray, base)
+                if corners is not None:
+                    card = get_card(img_gray, corners)
+                    card = cv2.flip(card,-1)
+                    captures.append(card)
+                    update_windows()
+                    #cv.ShowImage('card', card)
+                    has_moved = False
+                    been_to_base = False
+                    print "STATE: detected. waiting for go to base"
+        else:
+            if not has_moved:
+                print "STATE: has moved. waiting for stable"
+            has_moved = True
 
 
 def setup_windows():
-	cv2.namedWindow('card_1')
-	cv2.namedWindow('card_2')
-	cv2.namedWindow('card_3')
-	#cv.NamedWindow('base')
-	cv2.namedWindow('win')
-	#cv.StartWindowThread()
+    cv2.namedWindow('card_1')
+    cv2.namedWindow('card_2')
+    cv2.namedWindow('card_3')
+    #cv.NamedWindow('base')
+    cv2.namedWindow('win')
+    #cv.StartWindowThread()
 
 
 
@@ -190,11 +196,11 @@ corners should be close to [(167, 126), (384, 69), (460, 366), (235, 423)]
 
 '''
 for dirname, dirnames, filenames in os.walk('known'):
-	for filename in filenames:
-	path = os.path.join(dirname, filename)
-	img = cv.LoadImage(path,0)
-	cv.SetImageROI(img, (0,0,223,310))
-	known.append( (path, img) )
+    for filename in filenames:
+    path = os.path.join(dirname, filename)
+    img = cv.LoadImage(path,0)
+    cv.SetImageROI(img, (0,0,223,310))
+    known.append( (path, img) )
 
 
 
